421c4bad4ce654536d6d40b6a47485a831467ac5
namsu-k <nghokr1111@naver.com>
2022-10-19 23:52:11 +0900

    aaaa

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Home from "./routes/Home";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/movie/:id" element={<Detail />}></Route>
       </Routes>
     </Router>
